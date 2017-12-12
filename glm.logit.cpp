9794cf9b24b54240567c7158e690a9f583046662
Lucas Lochovsky <lucas.lochovsky@gmail.com>
2017-12-12 12:14:07 -0500

    Added missing supporting functions.

--- glm.logit.cpp
+++ glm.logit.cpp
@@ -45,7 +45,7 @@ static const double INVEPS = 1/DOUBLE_EPS;
 
 double x_d_omx(double x) {
   if (x < 0 || x > 1) {
-		fprintf(stderr, "Value %g out of range (0, 1)"), x);
+		fprintf(stderr, "Value %g out of range (0, 1)", x);
 		exit(1);
 	}
   return x/(1 - x);
@@ -56,6 +56,10 @@ double x_d_opx(double x) {return x/(1 + x);}
 int imin2(int x, int y)
 {
     return (x < y) ? x : y;
+}
+
+double R_D_exp (double x, bool log_p) {
+	return (log_p	? (x)	: exp(x));
 }
 
 // Returns the value ln[gamma(xx)] for xx > 0.
@@ -72,6 +76,66 @@ double gammln(double xx) {
 	ser=1.000000000190015;
 	for (j=0;j<=5;j++) ser += cof[j]/++y;
 	return -tmp+log(2.5066282746310005*ser/x);
+}
+
+double stirlerr(double n) {
+
+#define S0 0.083333333333333333333       /* 1/12 */
+#define S1 0.00277777777777777777778     /* 1/360 */
+#define S2 0.00079365079365079365079365  /* 1/1260 */
+#define S3 0.000595238095238095238095238 /* 1/1680 */
+#define S4 0.0008417508417508417508417508/* 1/1188 */
+
+/*
+  error for 0, 0.5, 1.0, 1.5, ..., 14.5, 15.0.
+*/
+    const static double sferr_halves[31] = {
+	0.0, /* n=0 - wrong, place holder only */
+	0.1534264097200273452913848,  /* 0.5 */
+	0.0810614667953272582196702,  /* 1.0 */
+	0.0548141210519176538961390,  /* 1.5 */
+	0.0413406959554092940938221,  /* 2.0 */
+	0.03316287351993628748511048, /* 2.5 */
+	0.02767792568499833914878929, /* 3.0 */
+	0.02374616365629749597132920, /* 3.5 */
+	0.02079067210376509311152277, /* 4.0 */
+	0.01848845053267318523077934, /* 4.5 */
+	0.01664469118982119216319487, /* 5.0 */
+	0.01513497322191737887351255, /* 5.5 */
+	0.01387612882307074799874573, /* 6.0 */
+	0.01281046524292022692424986, /* 6.5 */
+	0.01189670994589177009505572, /* 7.0 */
+	0.01110455975820691732662991, /* 7.5 */
+	0.010411265261972096497478567, /* 8.0 */
+	0.009799416126158803298389475, /* 8.5 */
+	0.009255462182712732917728637, /* 9.0 */
+	0.008768700134139385462952823, /* 9.5 */
+	0.008330563433362871256469318, /* 10.0 */
+	0.007934114564314020547248100, /* 10.5 */
+	0.007573675487951840794972024, /* 11.0 */
+	0.007244554301320383179543912, /* 11.5 */
+	0.006942840107209529865664152, /* 12.0 */
+	0.006665247032707682442354394, /* 12.5 */
+	0.006408994188004207068439631, /* 13.0 */
+	0.006171712263039457647532867, /* 13.5 */
+	0.005951370112758847735624416, /* 14.0 */
+	0.005746216513010115682023589, /* 14.5 */
+	0.005554733551962801371038690  /* 15.0 */
+    };
+    double nn;
+
+    if (n <= 15.0) {
+	nn = n + n;
+	if (nn == (int)nn) return(sferr_halves[(int)nn]);
+	return(gammln(n + 1.) - (n + 0.5)*log(n) + n - M_LN_SQRT_2PI);
+    }
+
+    nn = n*n;
+    if (n>500) return((S0-S1/nn)/n);
+    if (n> 80) return((S0-(S1-S2/nn)/nn)/n);
+    if (n> 35) return((S0-(S1-(S2-S3/nn)/nn)/nn)/n);
+    /* 15 < n <= 35 : */
+    return((S0-(S1-(S2-(S3-S4/nn)/nn)/nn)/nn)/n);
 }
 
 /* From R, currently only used for kode = 1, m = 1, n in {0,1,2,3} : */
@@ -428,6 +492,34 @@ double trigamma(double x)
     return ans;
 }
 
+double bd0(double x, double np) {
+    double ej, s, s1, v;
+    int j;
+
+    if (isnan(x) || isnan(np) || np == 0.0) {
+    	perror("Error: bd0 given an argument that is not a number.\n");
+			exit(1);
+		}
+
+    if (fabs(x-np) < 0.1*(x+np)) {
+	v = (x-np)/(x+np);  // might underflow to 0
+	s = (x-np)*v;/* s using v -- change by MM */
+	if(fabs(s) < DBL_MIN) return s;
+	ej = 2*x*v;
+	v = v*v;
+	for (j = 1; j < 1000; j++) { /* Taylor series; 1000: no infinite loop
+					as |v| < .1,  v^2000 is "zero" */
+	    ej *= v;// = v^(2j+1)
+	    s1 = s+ej/((j<<1)+1);
+	    if (s1 == s) /* last term was effectively 0 */
+		return s1 ;
+	    s = s1;
+	}
+    }
+    /* else:  | x - np |  is not too small */
+    return(x*log(x/np)+np-x);
+}
+
 /* A log-likelihood helper function */
 double loglik (int n, double th, 
 							 const vector<double> &mu, const vector<double> &y) {
@@ -447,8 +539,8 @@ double linkfun (double mu) {
 	return log(mu);
 }
 
-double linkinv (vector<double> &eta) {
-	double temp = (eta[i] < MTHRESH) ? DOUBLE_EPS : ((eta[i] > THRESH) ? INVEPS : exp(eta[i]));
+double linkinv (double eta) {
+	double temp = (eta < MTHRESH) ? DOUBLE_EPS : ((eta > THRESH) ? INVEPS : exp(eta));
 	return x_d_opx(temp);
 	// return (max(exp(eta), DBL_MIN));
 }
@@ -552,7 +644,7 @@ vector<double> logit_variance (vector<double> &mu) {
 // Returns TRUE if mu vector is within acceptable variance
 bool logit_validmu (vector<double> &mu) {
 	for (unsigned int i = 0; i < mu.size(); i++) {
-		if (isinf(mu[i]) || mu[i] <= 0.0 || mu >= 1.0) {
+		if (isinf(mu[i]) || mu[i] <= 0.0 || mu[i] >= 1.0) {
 			return false;
 		}
 	}
